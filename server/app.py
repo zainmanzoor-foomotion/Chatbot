a360a0ad2a9ecd98f828d21873a580083196f8d6
hasnainuser <hasnain@foomotion.io>
2026-04-06 17:09:12 +0500

    add vercel configuration for server

--- server/app.py
+++ server/app.py
@@ -20,7 +20,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://agentic-ai-chatbot-five.vercel.app"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
